dc19f683949b4334ff4d8fb29aba15fe6dd04610
gonpress <gonpress@naver.com>
2022-03-12 10:19:40 +0900

    tsconfig & add morgan

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,43 @@
 import express from 'express';
-import bodyParser from "body-parser";
+import * as bodyParser from "body-parser";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+import 'dotenv/config'
+import Controller from "./interfaces/controller.interface";
 
 class App {
     public app: express.Application;
-    public port: number;
 
-    constructor(controllers: any, port: number) {
+    constructor(controllers: Controller[]) {
         this.app = express();
-        this.port = port;
+
+        this.connectToTheDatabase();
 
         this.initMiddleWares();
         this.initControllers(controllers);
     }
 
     private initMiddleWares(){
+        this.app.use(morgan('dev'));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended:true}));
     }
 
-    private initControllers(controllers: any){
-        controllers.forEach((controller: any) => {
+    private initControllers(controllers: Controller[]){
+        controllers.forEach((controller: Controller) => {
             this.app.use('/', controller.router);
         })
     }
 
     public listen(){
-        this.app.listen(this.port, () => {
-            console.log(`App listening on the port ${this.port}`);
+        this.app.listen(process.env.PORT, () => {
+            console.log(`App listening on the port ${process.env.PORT}`);
         });
+    }
+
+    private connectToTheDatabase(){
+        mongoose.connect(`${process.env.MONGODB_URL}`);
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ import PostsController from "./posts/posts.controller";
 const app = new App(
     [
         new PostsController(),
-    ],
-    8000
+    ]
 )
 
 app.listen();
